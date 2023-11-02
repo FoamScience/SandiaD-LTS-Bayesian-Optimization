cbf7b1e56f74b18debebaf4865615d81cfa348ef
Elwardi <fadeli.elwardi@tu-darmstadt.de>
2023-11-02 12:19:30 +0100

    fix(case): minor fixes for base case

--- SandiaD_LTS/getImage.sh
+++ SandiaD_LTS/getImage.sh
@@ -4,13 +4,14 @@ cd "${0%/*}" || exit                                # Run from this directory
 #------------------------------------------------------------------------------
 casename=$(basename $PWD)
 if test -f "$PWD/$casename.png"; then
-    echo "$PWD/$casename.png"
+    cat $PWD/${casename}.imageUrl
 else
     touch case.foam
     pvpython renderResults.py --decomposed $casename
     convert  $casename.png -transparent white -trim -resize 90% $casename.png
-    echo "$PWD/$casename.png"
-    #curl -s --location --request POST "https://api.imgbb.com/1/upload?expiration=600&key=${IMGBB_API_KEY}"\
-    #--form "image=@./$casename.png" | jq .data.url | tee url
+    curl -s --location --request POST "https://api.imgbb.com/1/upload?expiration=600&key=${IMGBB_API_KEY}"\
+    --form "image=@./$casename.png" | jq .data.url | tee url
+    curl --upload-file "./$casename.png" "https://transfer.sh/$casename.png" > $PWD/${casename}.imageUrl
+    cat $PWD/${casename}.imageUrl
 fi
 #------------------------------------------------------------------------------
